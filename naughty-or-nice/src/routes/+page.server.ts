6aa8a7f820e7671bbeb55c70a39550940c5f8a57
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2024-12-05 00:55:42 +0100

    Made pageload server side to not expose api endpoint

--- naughty-or-nice/src/routes/+page.server.ts
+++ naughty-or-nice/src/routes/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageLoad } from "./$types"
+import type { PageServerLoad } from "./$types"
 
-export const load: PageLoad = async () => {
+export const load: PageServerLoad = async () => {
   const childList = await (await fetch("https://advent.sveltesociety.dev/data/2023/day-one.json")).json()
 
   return {
